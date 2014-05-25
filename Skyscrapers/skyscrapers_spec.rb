a4fa3e09905a0e0e75ab35e614b83277c447bfa8
Matt Sroufe <matt.sroufe@powershop.co.nz>
2014-05-25 19:29:03 +1200

    adds coordinates method

--- Skyscrapers/skyscrapers_spec.rb
+++ Skyscrapers/skyscrapers_spec.rb
@@ -1,21 +1,48 @@
+require 'byebug'
+
 class Building
   attr_reader :locations
 
   def initialize(locations)
     @locations = locations
   end
+
+  def coordinates
+    coords = []
+    (0...locations[1]).each do |i|
+      coords << [locations[0], i]
+    end
+
+    (locations[0]..locations[2]).each do |i|
+      coords << [i, locations[1]]
+    end
+
+    (0...locations[1]).to_a.reverse.each do |i|
+      coords << [locations[2], i]
+    end
+
+    coords
+  end
 end
 
 describe Building do
-  let(:building) { Building.new([1,2,3]) }
+  let(:building_1) { Building.new([1,2,3]) }
+  let(:building_2) { Building.new([2,4,6]) }
 
   it "is valid" do
-    expect(building).to be_an_instance_of Building
+    expect(building_1).to be_an_instance_of Building
   end
 
   describe "#locations" do
     it "returns the locations" do
-      expect(building.locations).to eq([1,2,3])
+      expect(building_1.locations).to eq([1,2,3])
+    end
+  end
+
+  describe "#coordinates" do
+    it "returns the buildings's coordinates" do
+      expect(building_1.coordinates).to eq([[1,0], [1,1], [1,2], [2,2], [3,2], [3,1], [3,0]])
+      expect(building_2.coordinates).to eq([[2,0], [2,1], [2,2], [2,3], [2,4], [3,4], [4,4], [5,4], [6,4], [6,3], [6,2], [6,1], [6,0]])
     end
   end
 end
